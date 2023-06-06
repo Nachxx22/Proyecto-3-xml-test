18885212ec06b23d7510c89b7a260e82333b8afb
Ignacio Lorenzo <49351085+Nachxx22@users.noreply.github.com>
2023-06-06 17:33:32 -0600

    Add files via upload

--- Proyecto3TestArduino/src/XMLreader.java
+++ Proyecto3TestArduino/src/XMLreader.java
@@ -1,6 +1,9 @@
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -10,13 +13,9 @@ import java.io.IOException;
 
 public class XMLreader {
     public static void main(String[] args) {
-        String NombreArchivo="Estudiantes";
-        // Especifica la ruta y el nombre del archivo XML que deseas crear
+        String nombreArchivo = "Estudiantes";
         String rutaProyecto = System.getProperty("user.dir");
-        String nombreArchivo=NombreArchivo;
-        CrearCarpeta.crearCarpeta(NombreArchivo);
-        String filePath = rutaProyecto + "/src/"+ NombreArchivo+"/"+ NombreArchivo+".xml";
-        //String filePath = "ruta/del/archivo/datos.xml"; // Cambiar la ruta y el nombre del archivo según sea necesario
+        String filePath = rutaProyecto + "/src/" + nombreArchivo + "/" + nombreArchivo + ".xml";
         readXML(filePath);
     }
 
@@ -29,23 +28,59 @@ public class XMLreader {
             document.getDocumentElement().normalize();
 
             NodeList rows = document.getElementsByTagName("row");
+            int numRows = rows.getLength();
+
+            // Obtener los nombres de las columnas
             Element headerRow = (Element) rows.item(0);
             NodeList headers = headerRow.getChildNodes();
+            int numCols = 0;
 
-            for (int i = 0; i < headers.getLength(); i++) {
-                System.out.print(headers.item(i).getNodeName() + "\t\t");
+            // Contar el número de nombres de columnas válidos
+            for (int j = 0; j < headers.getLength(); j++) {
+                String columnName = headers.item(j).getNodeName().trim();
+                if (!columnName.isEmpty() && !columnName.equals("#text")) {
+                    numCols++;
+                }
             }
-            System.out.println();
 
-            for (int i = 1; i < rows.getLength(); i++) {
+            // Crear la matriz para almacenar los datos
+            String[][] data = new String[numRows][numCols];
+
+            int colIndex = 0; // Índice de columna
+
+            // Agregar los nombres de las columnas a la primera fila de la matriz
+            for (int j = 0; j < headers.getLength(); j++) {
+                String columnName = headers.item(j).getNodeName().trim();
+                if (!columnName.isEmpty() && !columnName.equals("#text")) {
+                    data[0][colIndex] = columnName;
+                    colIndex++;
+                }
+            }
+
+            // Obtener los datos de las celdas y almacenarlos en la matriz
+            for (int i = 1; i < numRows; i++) {
                 Element row = (Element) rows.item(i);
                 NodeList cells = row.getChildNodes();
 
+                colIndex = 0; // Reiniciar el índice de columna
+
                 for (int j = 0; j < cells.getLength(); j++) {
-                    System.out.print(cells.item(j).getTextContent() + "\t\t");
+                    String cellData = cells.item(j).getTextContent().trim(); // Eliminar espacios en blanco
+                    if (!cellData.isEmpty() && !cellData.equals("#text")) { // Evitar datos innecesarios
+                        data[i][colIndex] = cellData;
+                        colIndex++;
+                    }
+                }
+            }
+
+            // Imprimir la matriz de datos
+            for (int i = 0; i < numRows; i++) {
+                for (int j = 0; j < numCols; j++) {
+                    System.out.print(data[i][j] + " ");
                 }
                 System.out.println();
             }
+
         } catch (ParserConfigurationException | SAXException | IOException e) {
             System.out.println("Error al leer el archivo XML: " + e.getMessage());
         }
